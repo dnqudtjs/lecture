37a9a443a694a9418436b339ab78bbd576cc9079
woo <dnqudtjs@gmail.com>
2020-03-12 06:54:24 +0900

    food example end

--- nomad/movie_app_2019/src/App.js
+++ nomad/movie_app_2019/src/App.js
@@ -1,24 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import PropTypes from "prop-types";
+
+const foodIlike = [
+  { id: 1, name: "aaa", image: "./aaa.jpg", rating: 3 },
+  { id: 2, name: "bbb", image: "./bbb.jpg", rating: 4 },
+  { id: 3, name: "ccc", image: "./ccc.jpg", rating: 5 },
+  { id: 4, name: "ddd", image: "./ddd.jpg", rating: 2 }
+];
+
+function Food({ name, picture, rating }) {
+  return (
+    <div>
+      <h1>I like {name}</h1>
+      <h4>{rating}/5.0</h4>
+      <img src={picture} alt={name} />
+    </div>
+  );
+}
+
+Food.propTypes = {
+  name: PropTypes.string.isRequired,
+  picture: PropTypes.string.isRequired,
+  rating: PropTypes.number.isRequired
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {foodIlike.map(dish => (
+        <Food
+          key={dish.id}
+          name={dish.name}
+          picture={dish.image}
+          rating={dish.rating}
+        />
+      ))}
     </div>
   );
 }
