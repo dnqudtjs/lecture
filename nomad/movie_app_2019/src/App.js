83cae0a8ec8985b2fa51fd42389afe7a1d23da10
woo <dnqudtjs@gmail.com>
2020-03-17 06:55:36 +0900

    before making Movie.js

--- nomad/movie_app_2019/src/App.js
+++ nomad/movie_app_2019/src/App.js
@@ -1,15 +1,24 @@
 import React from "react";
-import PropTypes from "prop-types";
+import axios from "axios";
 
 class App extends React.Component {
   state = {
-    isLoading: true
+    isLoading: true,
+    movies: []
+  };
+
+  getMovies = async () => {
+    // const movies = await axios.get("https://yts-proxy.now.sh/list_movies.json");
+    const {
+      data: {
+        data: { movies }
+      }
+    } = await axios.get("https://yts-proxy.now.sh/list_movies.json");
+    this.setState({ movies, isLoading: false });
   };
 
   componentDidMount() {
-    setTimeout(() => {
-      this.setState({ isLoading: false });
-    }, 3000);
+    this.getMovies();
   }
 
   render() {
