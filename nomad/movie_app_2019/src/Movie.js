b89edc1f5bc22460231236b36d60fdad71ee72fa
woo <dnqudtjs@gmail.com>
2020-03-19 06:29:42 +0900

    #4.3 Adding Genres

--- nomad/movie_app_2019/src/Movie.js
+++ nomad/movie_app_2019/src/Movie.js
@@ -2,13 +2,20 @@ import React from "react";
 import PropTypes from "prop-types";
 import "./Movie.css";
 
-function Movie({ year, title, summary, poster }) {
+function Movie({ year, title, summary, poster, genres }) {
   return (
     <div class="movie">
       <img src={poster} alt={title} title={title} />
       <div class="movie_data">
         <h3 class="movie_title">{title}</h3>
         <h5 class="movie_year">{year}</h5>
+        <ul className="genres">
+          {genres.map((genre, index) => (
+            <li key={index} className="genres_genre">
+              {genre}
+            </li>
+          ))}
+        </ul>
         <p class="movie_summary">{summary}</p>
       </div>
     </div>
@@ -20,7 +27,8 @@ Movie.propTypes = {
   year: PropTypes.number.isRequired,
   title: PropTypes.string.isRequired,
   summary: PropTypes.string.isRequired,
-  poster: PropTypes.string.isRequired
+  poster: PropTypes.string.isRequired,
+  genres: PropTypes.arrayOf(PropTypes.string).isRequired
 };
 
 export default Movie;
