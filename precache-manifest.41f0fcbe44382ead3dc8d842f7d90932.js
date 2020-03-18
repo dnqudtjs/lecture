749fff71de513512ee9bfffc6d268ea45c82196c
woo <dnqudtjs@gmail.com>
2020-03-19 08:11:15 +0900

    Updates

---
+++ precache-manifest.41f0fcbe44382ead3dc8d842f7d90932.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1444a4ba3e071cbc1838ffe07275f05b",
+    "url": "/lecture/index.html"
+  },
+  {
+    "revision": "69043b017e82f9729f46",
+    "url": "/lecture/static/css/main.10b0ebdd.chunk.css"
+  },
+  {
+    "revision": "86325af0249feaa2a644",
+    "url": "/lecture/static/js/2.9ad0a64e.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/lecture/static/js/2.9ad0a64e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "69043b017e82f9729f46",
+    "url": "/lecture/static/js/main.4bdace60.chunk.js"
+  },
+  {
+    "revision": "32594dae1336578ed18d",
+    "url": "/lecture/static/js/runtime-main.013e0756.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/movie_app_2019/precache-manifest.4f323b0b3359b5162378e8e3a9735d0b.js"
+  "/lecture/precache-manifest.41f0fcbe44382ead3dc8d842f7d90932.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/movie_app_2019/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/lecture/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
