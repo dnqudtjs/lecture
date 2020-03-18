4b6fcbc358979b13d3c26fcffd4eacd504ee0b57
woo <dnqudtjs@gmail.com>
2020-03-19 07:06:24 +0900

    #4.5 Cutting the summary

--- nomad/movie_app_2019/src/Movie.js
+++ nomad/movie_app_2019/src/Movie.js
@@ -7,16 +7,16 @@ function Movie({ year, title, summary, poster, genres }) {
     <div className="movie">
       <img src={poster} alt={title} title={title} />
       <div className="movie_data">
-        <h3 className="movie_title">{title}</h3>
-        <h5 className="movie_year">{year}</h5>
-        <ul className="genres">
+        <h3 className="movie__title">{title}</h3>
+        <h5 className="movie__year">{year}</h5>
+        <ul className="movie__genres">
           {genres.map((genre, index) => (
             <li key={index} className="genres_genre">
               {genre}
             </li>
           ))}
         </ul>
-        <p className="movie_summary">{summary}</p>
+        <p className="movie_summary">{summary.slice(0, 180)}...</p>
       </div>
     </div>
   );
